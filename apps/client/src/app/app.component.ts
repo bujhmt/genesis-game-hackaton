bd5ae28a66b934bb322bb74d7ca21b69e2e0b7cf
Igor Fenchenko <igor.fenchenko@traffickitchen.io>
2023-02-10 19:38:10 +0200

    sockets ready

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,26 +1,32 @@
-import {Component, OnInit} from '@angular/core';
-import {Store} from '@ngxs/store';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Select, Store} from '@ngxs/store';
 import {SetTank} from './state/tanks.actions';
 import {Events} from '@game/events';
 import {Direction, Tank} from '@game/models';
-import {io} from 'socket.io-client';
+import {io, Socket} from 'socket.io-client';
 import {environment} from '../environments/environment';
+import {Observable, Subscription} from 'rxjs';
+import {TanksState} from './state/tanks.state';
 
 @Component({
   selector: 'game-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = [];
+  public socket: Socket;
+  @Select(TanksState.tanks) public tanks$: Observable<Tank[]>;
+
   constructor(
     private readonly store: Store,
   ) {
   }
 
   ngOnInit() {
-    const socket = io(environment.baseUrl)
+    this.socket = io(environment.baseUrl)
 
-    socket.on('connect', () => {
+    this.socket.on('connect', () => {
       console.log('Connected to server!');
 
       const x = Math.ceil(Math.random() * 80 + 10);
@@ -31,16 +37,36 @@ export class AppComponent implements OnInit {
         y: y - (y % 20),
       };
 
-      socket.emit(Events.SAVE, {
-        id: socket.id,
+      this.socket.emit(Events.SAVE, {
+        id: this.socket.id,
         direction: Direction.TOP,
         position
       });
 
-      socket.on(Events.SYNCHRONIZE, (tanks: Tank[]) => {
+      this.socket.on(Events.SYNCHRONIZE, (tanks: Tank[]) => {
         console.log(`${Events.SYNCHRONIZE} event!`);
         this.store.dispatch(tanks.map((tank) => new SetTank(tank)));
       })
     });
+
+    this.subscriptions.push(
+      this.tanks$.subscribe((tanks) => {
+        const clientTank = tanks.find((tank) => tank.id === this.socket.id);
+
+        if (!clientTank) {
+          return;
+        }
+
+        this.socket.emit(Events.SAVE, clientTank);
+      })
+    )
+  }
+
+  ngOnDestroy() {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+
+    this.socket.close();
   }
 }
